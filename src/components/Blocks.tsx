39bef82f4eadb3df03e73c934f4f6354bb994310
leadingmind <leadingmind@users.noreply.github.com>
2020-01-19 12:18:31 +0530

    Sq

--- src/components/Blocks.tsx
+++ src/components/Blocks.tsx
@@ -3,11 +3,24 @@ import { Flex, Box } from "rebass";
 
 export default () => (
   <Flex flexWrap="wrap">
-    <Box bg="papayawhip" color="purple" p={4} width={[1 / 2, 1 / 3, 1 / 4]}>
+    <Box
+      border={6}
+      borderColor="black"
+      bg="papayawhip"
+      color="purple"
+      p={4}
+      width={[1 / 2, 1 / 3, 1 / 4]}
+    >
       One
     </Box>
     <Box bg="papayawhip" color="purple" p={4} width={[1 / 2, 1 / 3, 1 / 4]}>
       Two
+    </Box>
+    <Box bg="papayawhip" color="purple" p={4} width={[1 / 2, 1 / 3, 1 / 4]}>
+      Three
+    </Box>
+    <Box bg="papayawhip" color="purple" p={4} width={[1 / 2, 1 / 3, 1 / 4]}>
+      Four
     </Box>
   </Flex>
 );
